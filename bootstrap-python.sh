6e3029a7b657e71819bb070d6a65093b10824d28
learnmews <learnmews@users.noreply.github.com>
2017-05-29 13:30:41 -0500

    updated boostrap scripts

--- bootstrap-python.sh
+++ bootstrap-python.sh
@@ -4,24 +4,30 @@ function install {
     echo installing $1
     shift
     apt-get -y install "$@" >/dev/null 2>&1
-}
+} 
+
+function pipinstall {
+    echo installing $1
+    shift
+    pip3 install --upgrade "$@" >/dev/null 2>&1
+} 
 
 echo updating python package information
 add-apt-repository -y ppa:mystic-mirage/pycharm >/dev/null 2>&1
 apt-get -y update >/dev/null 2>&1
 
 echo installing python tools
-apt-get install -y python3-dev python3-pip 
-apt-get install -y bpython3
-apt-get install -y idle3
-apt-get install -y pycharm
-pip3 install --upgrade pip3
-pip3 install --upgrade virtualenv 
-pip3 install --upgrade virtualenvwrapper
-pip3 install --upgrade jupyter
-pip3 install --upgrade ipython
+install python3-dev python3-pip 
+install bpython3
+install idle3
+install pycharm
+pipinstall pip3
+pipinstall virtualenv 
+pipinstall virtualenvwrapper
+pipinstall jupyter
+pipinstall ipython
 
 # echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> /home/vagrant/.bashrc
 # echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/vagrant/.bashrc
-
+echo 'python setup complete'
 echo 'all set, rock on!'
